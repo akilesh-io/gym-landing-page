58fd126cf866ad0be01d3cd7aa4b9c1d2889c131
Akilesh <53332156+2112akilesh@users.noreply.github.com>
2023-08-07 20:34:34 +0530

    image updated

--- components/gallery/photos.ts
+++ components/gallery/photos.ts
@@ -1,5 +1,6 @@
   const breakpoints = [694, 640, 384, 256, 128, 96, 64, 48];
 
+
   const pic = [
     {
       src: "/1.jpg",
@@ -8,48 +9,63 @@
     },
     {
       src: "/2.jpg",
-      width: 1000,
-      height: 450,
+      width: 750,
+      height: 1124,
     },
     {
       src: "/3.jpg",
-      width: 1440,
-      height: 1440,
+      width: 750,
+      height: 1124,
+    },
+    // {
+    //   src: "/4.jpg",
+    //   width: 1520,
+    //   height: 1027,
+    // },
+    // {
+    //   src: "/5.jpg",
+    //   width: 4233,
+    //   height: 2816,
+    // },
+    {
+      src: "https://lh5.googleusercontent.com/p/AF1QipNeQVHY9i14LiBVN7GuexixmQar4hUxRSo1JaOh=w1520-h1027-k-no",
+      width: 1520,
+      height: 1027,
     },
     {
-      src: "/4.jpg",
-      width: 1440,
-      height: 1440,
+      src: "https://lh5.googleusercontent.com/p/AF1QipPWcLMMXZ-DBsqNmIGaT7Gu4Wpep4cmkqJZw1Ec=w1520-h1027-k-no",
+      width: 1520,
+      height: 1027,
     },
     {
-      src: "/5.jpg",
-      width: 1080,
-      height: 1080,
+      src: "https://lh5.googleusercontent.com/p/AF1QipM7kKlVgsJ7UkrIExtrhECvq6yATP47OvILB9XY=w1520-h1027-k-no",
+      width: 1520,
+      height: 1027,
     },
     {
-      src: "/6.jpg",
-      width: 1080,
-      height: 1350,
+      src: "https://lh5.googleusercontent.com/p/AF1QipNeQVHY9i14LiBVN7GuexixmQar4hUxRSo1JaOh=w1520-h1027-k-no",
+      width: 1520,
+      height: 1027,
     },
     {
-      src: "/7.jpg",
-      width: 1080,
-      height: 1080,
+      src: "https://lh5.googleusercontent.com/p/AF1QipPou9guINyO7kQTkWbd3KKhJqQiBEps7myvMg70=w406-h506-k-no",
+      width: 406,
+      height: 506,
     },
     {
-      src: "/8.jpg",
-      width: 1080,
-      height: 566,
+      src: "https://lh5.googleusercontent.com/p/AF1QipPYRSxBVztOudf5ZGIgiqxJK-VMpmPHQ_4ajR-a=w406-h508-k-no",
+      width: 406,
+      height: 508,
     },
     {
-      src: "/9.jpg",
-      width: 1080,
-      height: 566,
+      src: "https://lh5.googleusercontent.com/p/AF1QipOTZ4gbj-MvAvw1mGoBJz2qBO8Lbh5e5pU3f2c=w406-h338-k-no",
+      width: 406,
+      height: 338,
     },
     {
-      src: "/10.jpg",
-      width: 1080,
-      height: 566,
+      src: "https://lh5.googleusercontent.com/p/AF1QipP1XKN4rOvetsIZ9rG2ur4BE-BiEvsfSTQAMc8=w406-h508-k-no",
+      width: 406,
+      height: 338,
     },
   ];
   
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,12 @@ import type {AppProps} from 'next/app';
 import {createTheme, NextUIProvider} from '@nextui-org/react';
 import {ThemeProvider as NextThemesProvider} from 'next-themes';
 
-// const lightTheme = createTheme({
-//    type: 'light',
-//    theme: {
-//       colors: {},
-//    },
-// });
+const lightTheme = createTheme({
+   type: 'light',
+   theme: {
+      colors: {},
+   },
+});
 
 const darkTheme = createTheme({
    type: 'dark',
@@ -22,10 +22,9 @@ function MyApp({Component, pageProps}: AppProps) {
    return (
       <NextThemesProvider
          attribute="class"
-         defaultTheme = "dark"
          themes ={['dark']}
          value={{
-           //light: lightTheme.className,
+         //light: lightTheme.className,
             dark: darkTheme.className,
          }}
       >
